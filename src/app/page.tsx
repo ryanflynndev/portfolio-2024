ac55c95f8b1b1bdb125170cd299f8f5dbe38b710
Ryan Flynn <ryanflynn44@gmail.com>
2024-06-04 11:57:20 -0400

    fix: build errs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function Home() {
   const [title, setTitle] = useState(0);
 
-  const titles = ['code', 'design', 'build', 'create'];
+  const titles = ["code", "design", "build", "create"];
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -16,7 +16,7 @@ export default function Home() {
       
     }, 3000);
     return () => clearInterval(interval);
-  }, []);
+  }, [title]);
 
   if (title > 3) {
     setTitle(title => title = 0);
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     <div className="h-screen w-screen bg-black overflow-auto overflow-x-hidden">
       <Navbar />
-      <div className='flex flex-col md:flex-row mx-auto min-h-screen max-w-6xl items-center md:items-start py-40'>
+      <div className="flex flex-col md:flex-row mx-auto min-h-screen max-w-6xl items-center md:items-start py-40">
         <div className="pt-32 md:pt-20 flex-1 animate-float-in-left">
           <h2 className="font-bold text-6xl text-white font-inter">
             Hi! 👋
@@ -40,8 +40,8 @@ export default function Home() {
               great experiences.
             </h3>
           </div>
-          <p className='pt-8 text-white pb-4'>
-            I'm currently a full-stack engineer at Envoy where I help build the future of the workplace. I'm passionate about building products that help make people's lives easier. I'm also a huge fan of design and love to create beautiful and functional interfaces.
+          <p className="pt-8 text-white pb-4">
+            {"I'm currently a full-stack engineer at Envoy where I help build the future of the workplace. I'm passionate about building products that help make people's lives easier. I'm also a huge fan of design and love to create beautiful and functional interfaces."}
           </p>
           <a
             href="https://github.com/ryanflynndev" // Replace with your GitHub profile link
@@ -53,13 +53,13 @@ export default function Home() {
           </a>
         </div>
         <div className="relative flex-1 flex justify-center md:justify-end md:pl-8 animate-float-in-right">
-          <div className='relative z-20 p-1 pt-8'>
+          <div className="relative z-20 p-1 pt-8">
             <Image
               height={400}
               width={400}
-              src='/pngs/me.png'
-              alt='Picture of Ryan Flynn'
-              className='rounded-md object-cover shadow-lg border-1'
+              src="/pngs/me.png"
+              alt="Picture of Ryan Flynn"
+              className="rounded-md object-cover shadow-lg border-1"
             />
           </div>
           <div className="absolute top-6 left-16 w-[400px] h-[400px] border-4 border-purple-300 hover:bg-purple-300 rounded-md z-10"></div>
